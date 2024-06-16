8d376e5b2eb806d51d92c16042e80de45d87da55
Agnija Ravdo <41111594+agnijaravdo@users.noreply.github.com>
2024-06-16 20:02:33 +0300

    add enter activity date feature in log activities mode

--- modes/log_activities_and_hours_mode.py
+++ modes/log_activities_and_hours_mode.py
@@ -1,2 +1,25 @@
+import datetime
+
+from utils import clear_screen, print_heading
+
+
 def log_activities_and_hours():
-    print("hi")
+    print_heading("Log Activities And Hours Spent on Them")
+    print("⬅️ To go back to the main menu, press Ctrl+C or Ctrl+D. To close the program, press Ctrl+Z.\n")
+    logged_date = enter_activity_date()
+
+def enter_activity_date():
+    while True:
+        try:
+            date = input("Enter a date to log your activity (YYYY-MM-DD format): ")
+            year, month, day = map(int, date.split("-"))
+
+            return datetime.date(year, month, day)
+
+        except ValueError as e:
+            print(
+                f"{str(e).capitalize()}. You can only enter date in YYYY-MM-DD format. Please try again"
+            )
+        except (KeyboardInterrupt, EOFError):
+            clear_screen()
+            break
