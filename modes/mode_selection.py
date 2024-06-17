5f3291fb0f4ffa3d5aae8710f3cdcde484d101aa
Agnija Ravdo <41111594+agnijaravdo@users.noreply.github.com>
2024-06-17 21:32:15 +0300

    improve personalised week analysis

--- modes/mode_selection.py
+++ modes/mode_selection.py
@@ -2,8 +2,9 @@ from enum import Enum
 from simple_term_menu import TerminalMenu
 
 from category import Category
-from modes.display_data import display_total_logged_in_hours_for_each_category
+from modes.display_performance_data import display_total_logged_in_hours_for_each_category
 from modes.log_activities_and_hours_mode import log_activities_and_hours
+from modes.personalized_week_analysis import show_personalised_week_analysis
 from modes.set_categories_and_target_hours_mode import enter_categories_and_target_hours
 from modes.show_categories_and_target_hours_mode import show_categories_and_target_hours
 from modes.show_weekly_strava_activities import show_weekly_strava_activities
@@ -15,7 +16,7 @@ class StartMenuItem(Enum):
     SHOW_CATEGORIES_AND_TARGET_HOURS = "Show categories and target hours"
     LOG_ACTIVITIES_AND_HOURS = "Log activities and hours"
     SHOW_WEEKLY_STRAVA_ACTIVITIES = "Show weekly Strava activities"
-    DISPLAY_DATA = "Display data"
+    GENERATE_PERFORMANCE_DATA = "Generate weekly and daily performance data"
     PERSONALIZED_WEEK_ANALYSIS = "Personalized week analysis"
     EXIT_PROGRAM = "Exit program"
 
@@ -45,12 +46,10 @@ def show_mode_selection_menu():
             validate_categories_count(log_activities_and_hours, selected_mode)
         elif selected_mode == StartMenuItem.SHOW_WEEKLY_STRAVA_ACTIVITIES:
             validate_categories_count(show_weekly_strava_activities, selected_mode)
-        elif selected_mode == StartMenuItem.DISPLAY_DATA:
+        elif selected_mode == StartMenuItem.GENERATE_PERFORMANCE_DATA:
             validate_activities_count(display_total_logged_in_hours_for_each_category, selected_mode)
         elif selected_mode == StartMenuItem.PERSONALIZED_WEEK_ANALYSIS:
-            print(
-                "Validate if categories and activities not null and display personalized week analysis"
-            )
+            validate_activities_count(show_personalised_week_analysis, selected_mode)
         elif selected_mode == StartMenuItem.EXIT_PROGRAM:
             print("Exiting program. Goodbye!👋")
             break
